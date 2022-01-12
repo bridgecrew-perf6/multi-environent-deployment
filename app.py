d4d9dff9af37f5eecd52af32324d75b64e775539
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-01-12 11:17:23 +0700

    complete

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from multi_environent_deployment.multi_environent_deployment_stack import MultiE
 
 app = cdk.App()
 
-# dev_account = app.node.try_get_context('envs')['dev']
-# prod_account = app.node.try_get_context('envs')['prod']
+dev_account = app.node.try_get_context('envs')['dev']
+prod_account = app.node.try_get_context('envs')['prod']
 
-# env_AUS = cdk.Environment(region=dev_account['region'], account=dev_account['account'])
-# env_ASIA = cdk.Environment(region=prod_account['region'], account=prod_account['account'])
+env_AUS = cdk.Environment(region=dev_account['region'], account=dev_account['account'])
+env_ASIA = cdk.Environment(region=prod_account['region'], account=prod_account['account'])
 
-# MultiEnvironentDeploymentStack(app, "myDevStack", is_prod=False, env=env_AUS)
-# MultiEnvironentDeploymentStack(app, "myProdStack", is_prod=True, env=env_ASIA)
+MultiEnvironentDeploymentStack(app, "myDevStack", is_prod=False, env=env_AUS)
+MultiEnvironentDeploymentStack(app, "myProdStack", is_prod=True, env=env_ASIA)
 
 app.synth()
