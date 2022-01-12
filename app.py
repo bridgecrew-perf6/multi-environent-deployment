80c7fa9934ba43452882a8d634a96575478c0c6c
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-01-12 13:45:31 +0700

    add tags

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ from resource_stacks.custom_vpc import CustomVpcStack
 
 app = cdk.App()
 
-CustomVpcStack(app, "my-custom-vpc-stack")
+prod_configs = app.node.try_get_context('envs')['prod']
+prod_env=cdk.Environment(region=prod_configs['region'], account=prod_configs['account'])
+
+CustomVpcStack(app, "my-custom-vpc-stack", env=prod_env)
+
+cdk.Tags.of(app).add("stack-team-support-email", prod_configs['stack-team-support-email'])
+cdk.Tags.of(app).add("stack-level-tagging", "sample_tag_value")
 
 app.synth()
--- resource_stacks/custom_vpc.py
+++ resource_stacks/custom_vpc.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
     Stack,
+    Tags,
     aws_ec2 as ec2,
+    aws_s3 as s3,
     CfnOutput
 )
 from constructs import Construct
@@ -33,4 +35,24 @@ class CustomVpcStack(Stack):
                     subnet_type=ec2.SubnetType.PRIVATE_ISOLATED)
             ])
         
+        Tags.of(custom_vpc).add("Owner", "g025Vpc")
+        
         CfnOutput(self, "customVpcOutput", value=custom_vpc.vpc_id, export_name="customVpcId")
+
+        my_bucket = s3.Bucket(self, "customBucketId-g025")
+
+        Tags.of(my_bucket).add("Owner", "g025Vpc")
+
+        # Resource in same account.
+        bucket1 = s3.Bucket.from_bucket_name(self, "generalman025-s3-inventory", "generalman025-s3-inventory")
+
+        bucket2 = s3.Bucket.from_bucket_arn(self, "thebucket", "arn:aws:s3:::thebucketofgeneralman025")
+
+        CfnOutput(self, "generalman025-s3-inventoryId", value=bucket1.bucket_name, export_name="generalman025-s3-inventory")
+
+        vpc2 = ec2.Vpc.from_lookup(self, "vpc2", vpc_id="vpc-08c8ec9e921deff16")
+
+        CfnOutput(self, "importedVpc2", value=vpc2.vpc_id, export_name="importedVpc2")
+
+        peer_vpc = ec2.CfnVPCPeeringConnection(self, "peerVpc", peer_vpc_id=custom_vpc.vpc_id, vpc_id=vpc2.vpc_id)
+
