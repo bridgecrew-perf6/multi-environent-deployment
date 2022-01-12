1bdd4a20f7dc095be4236c3bacb82b82c413c9ac
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-01-12 22:49:02 +0700

    completed

--- bootstrap_scripts/install_httpd.sh
+++ bootstrap_scripts/install_httpd.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-sudo yum install -y httpd
-sudo chkconfig httpd on
-sudo service httpd start
+yum update -y
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+yum install -y httpd mariadb-server
+systemctl start httpd
+systemctl enable httpd
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www 
+find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
